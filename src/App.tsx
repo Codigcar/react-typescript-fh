7d431d0d394a75fdc0f85742df56d87d4bc7aa83
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-05-30 17:12:41 -0500

    useEffect + useRef + Custom Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Counter } from './components/Counter';
+import { TimerPadre } from './components/TimerPadre';
+import { Usuario } from './components/Usuario';
 
 function App() {
   return (
     <>
       <Counter />
+      <hr />
+      <Usuario />
+      <hr />
+      <TimerPadre />
+
     </>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Usuario } from './Usuario';
 
 export const Counter = () => {
     const [counter, setCounter] = useState(0)
@@ -20,7 +19,6 @@ export const Counter = () => {
             <button onClick={ () => incrementar()} className="btn btn-outline-primary mt-2">+1</button>
             <button onClick={ () => incrementar(2)} className="btn btn-outline-warning mt-2">+2</button>
             <button onClick={ () => reset()} className="btn btn-outline-danger mt-2">Reset</button>
-            <Usuario />
         </div>
         </>
     )
